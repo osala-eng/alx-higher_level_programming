94534a984d4b59d4471098a8a17d23710589aa74
Jashon Osala <jashonosala@gmail.com>
2022-09-19 15:58:51 +0300

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for a cycle in a list
+ * @list: list
+ * Return: cycle ? 1 : 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *chaser, *runner;
+
+	if (!list || !list->next)
+		return (0);
+
+	chaser = list->next;
+	runner = (list->next)->next;
+
+	for (; runner;)
+	{
+		if (chaser == runner)
+		{
+			chaser = list;
+			for (; chaser != runner;)
+			{
+				chaser = chaser->next;
+				runner = runner->next;
+			}
+			return (1);
+		}
+
+		chaser = chaser->next;
+		runner = (runner->next)->next;
+	}
+
+	return (0);
+}
