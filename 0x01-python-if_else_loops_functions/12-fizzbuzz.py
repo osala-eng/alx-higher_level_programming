18eb4b6d88b782a0d685f711d2afe8b86e016614
Jashon Osala <jashonosala@gmail.com>
2022-09-20 16:08:09 +0300

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,8 @@
 def fizzbuzz():
     for i in range(1, 101):
         k = i % 5 == 0
-        l = i % 3 == 0
-        j = k and l
-        mes = f"FizzBuzz" if j else f"Buzz" if k else f"Fizz" if l else f"{i:d}"
+        m = i % 3 == 0
+        j = k and m
+        mes = f"FizzBuzz" if j else f"Buzz" if k \
+            else f"Fizz" if m else f"{i:d}"
         print(mes, end=" ")
