4e4e7973cd7d2c534b1b70d85fc67a643d9cd84c
Jashon Osala <jashonosala@gmail.com>
2022-09-20 15:47:25 +0300

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
 mes = f"null"
-
 last = number % 10 if number > 0 else (-1 * number) % 10
 if last != 0:
     mes = f"and is greater than 5" if last > 5 \
