d115ae6590136ea47740c00c5608ea9178f83a59
Jashon Osala <jashonosala@gmail.com>
2022-09-20 19:22:25 +0300

    Add format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             i = chr(ord(i) - ord('a') + ord('A'))
-        print("{:s}".formart(i), end="")
-    print()
+        print("{:s}".format(i), end="")
+    print("{}".format(""))
