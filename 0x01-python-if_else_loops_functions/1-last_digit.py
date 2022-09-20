60c760b8c71bcae74f6e4994e7a544f0647c15af
Jashon Osala <jashonosala@gmail.com>
2022-09-20 18:52:36 +0300

    Fix negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 mes = f"null"
-last = number % 10 if number > 0 else (-1 * number) % 10
+last = number % 10 if number > 0 else number % -10
 if last != 0:
     mes = f"and is greater than 5" if last > 5 \
             else f"and is less than 6 and not 0"
