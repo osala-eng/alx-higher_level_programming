288107fbb356be3faae6a2b6c3bef63babf30089
Jashon Osala <jashonosala@gmail.com>
2022-09-20 19:13:28 +0300

    Edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-i = 0
+i = 1
 while i < 100:
-    if i > 0 and i % 10 == 0:
+    if i % 10 == 0:
         i = int(i + (i / 10) + 1)
         continue
     print("{:02d}".format(i), end=", " if i < 89 else "\n")
