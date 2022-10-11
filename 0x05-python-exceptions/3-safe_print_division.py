481dfed026c8219916242419b45e9cf99f37e595
Jashon Osala <jashonosala@gmail.com>
2022-10-12 00:06:23 +0300

    Debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     quot = None
     try:
-        quot = a / b;
-    except:
+        quot = a / b
+    except (ZeroDivisionError, TypeError):
         pass
     finally:
         print("Inside result: {}".format(quot))
