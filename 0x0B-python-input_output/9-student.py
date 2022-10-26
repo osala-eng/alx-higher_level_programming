56b27e37202ef1d6112cee01017ffe770fe1a53b
Jashon Osala <jashonosala@gmail.com>
2022-10-26 05:03:55 +0300

    Class student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,8 +3,7 @@
 
 Contains class Student
 that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that returns dictionary representation
-of requested attributes or all if none were requested
+and has public method to_json that retrieves its dictionary representation
 """
 
 
@@ -26,29 +25,10 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         """
         Returns dictionary description with simple data structure
         (list, dictionary, dictionary, string)
         for JSON serialization of an object
-
-        Return:
-            Only return dict of attrs given to us
-            Return entire dict if no attrs given
         """
-        if attrs is None:
-            return self.__dict__
-        else:
-            dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
-
-    def reload_from_json(self, json):
-        """
-        Return:
-            Transfer all attributes of json to self
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        return self.__dict__
