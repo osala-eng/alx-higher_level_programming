c32f7671bdc84be9e3e90aa0fefc6093c5b968cb
Jashon Osala <jashonosala@gmail.com>
2022-12-19 15:33:17 +0300

    Modify compare function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const nums = process.argv.slice(2);
-  nums.sort((a, b) => a < b);
+  nums.sort((a, b) => b - a);
   console.log(nums[1]);
 }
